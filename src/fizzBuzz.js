0889b825d3404b25a7f27ff4ff40eaf2e987ed23
SiobhanGi <siobhan.goggins@gmail.com>
2018-05-14 16:31:03 +0100

    refactored fizzbuzz.js

--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -1,14 +1,18 @@
 function Fizzbuzz() { }
 
 Fizzbuzz.prototype.choose = function(number){
-  if (number % 3 === 0 && number % 5 === 0) {
+  if (this._isDivisible(15, number)) {
     return 'fizzbuzz';
-  } else if (number % 3 === 0) {
+  } else if (this._isDivisible(3, number)) {
     return 'fizz';
-  } else if (number % 5 === 0) {
+  } else if (this._isDivisible(5, number)) {
     return 'buzz';
   } else {
     return number;
   }
 
+}
+
+Fizzbuzz.prototype._isDivisible = function(diviser, number) {
+  return (number % diviser === 0 );
 }
